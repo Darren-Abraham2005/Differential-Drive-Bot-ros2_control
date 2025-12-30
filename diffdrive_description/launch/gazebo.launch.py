6d806984ca7b4799afd2955f37d624613b13d058
Darren-Abraham2005 <darrenpaulabraham@gmail.com>
2025-12-30 17:09:23 +0530

    First commit

---
+++ diffdrive_description/launch/gazebo.launch.py
@@ -0,0 +1,94 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration,PythonExpression
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.actions import AppendEnvironmentVariable, DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+import xacro
+from os.path import join
+
+def generate_launch_description():
+
+    # Package Directories
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    pkg_ros_gz_rbot = get_package_share_directory('diffdrive_description')
+    world_path = os.path.join(
+    get_package_share_directory('diffdrive_description'),
+    'worlds',
+    'obstacles.sdf' )
+    resource_path = os.path.join(pkg_ros_gz_rbot, '..')
+    set_gz_resource_path = AppendEnvironmentVariable(
+    'GZ_SIM_RESOURCE_PATH',
+    resource_path
+)
+
+    # Parse robot description from xacro
+    robot_description_file = os.path.join(pkg_ros_gz_rbot, 'urdf', 'diff_drive.xacro')
+    ros_gz_bridge_config = os.path.join(pkg_ros_gz_rbot, 'config', 'ros_gz_bridge_gazebo.yaml')
+    
+    robot_description_config = xacro.process_file(
+        robot_description_file
+    )
+    robot_description = {'robot_description': robot_description_config.toxml()}
+
+
+    # Start Robot state publisher
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='both',
+        parameters=[robot_description],
+    )
+
+    # Start Gazebo Sim
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")),
+        launch_arguments={
+            "gz_args": f'-r -v 4 {world_path}'
+        }.items()
+    )
+
+    # Spawn Robot in Gazebo   
+    spawn = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            "-topic", "/robot_description",
+            "-name", "diffdrive_bot",
+            "-allow_renaming", "true",
+            "-z", "0.32",
+            "-x", "0.0",
+            "-y", "0.0",
+            "-Y", "0.0"
+        ],            
+        output='screen',
+    )
+
+    # Bridge ROS topics and Gazebo messages for establishing communication
+    start_gazebo_ros_bridge_cmd = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        parameters=[{
+          'config_file': ros_gz_bridge_config,
+        }],
+        output='screen'
+    )
+
+    
+
+    return LaunchDescription(
+        [
+            # Nodes and Launches
+            set_gz_resource_path,
+            gazebo,
+            spawn,
+            start_gazebo_ros_bridge_cmd,
+            robot_state_publisher,
+        
+        ]
+    )
